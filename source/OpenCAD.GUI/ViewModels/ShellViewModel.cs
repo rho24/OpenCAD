1d07024a24c746eeee6ef76fac811207290757fa
Richard Oliver <richard@rholiver.co.uk>
2013-02-24 13:29:18 +0000

    mvvmified tools

--- source/OpenCAD.GUI/ViewModels/ShellViewModel.cs
+++ source/OpenCAD.GUI/ViewModels/ShellViewModel.cs
@@ -1,12 +1,11 @@
-﻿using AvalonDock.Layout;
-using Caliburn.Micro;
+﻿using Caliburn.Micro;
 
 namespace OpenCAD.GUI.ViewModels
 {
     public class ShellViewModel : Conductor<Screen>
     {
         private Screen _activeDocument;
-        public BindableCollection<Screen> Tabs { get; set; }
+
         public Screen ActiveDocument {
             get { return _activeDocument; }
             set {
@@ -16,14 +15,23 @@ namespace OpenCAD.GUI.ViewModels
             }
         }
 
+        public BindableCollection<Screen> Tabs { get; set; }
+        public BindableCollection<Screen> Tools { get; set; }
+
         public ShellViewModel() {
             Tabs = new BindableCollection<Screen>();
+            Tools = new BindableCollection<Screen>();
         }
 
         public void AddTeapot() {
             var model = new TeapotViewModel {Title = "Teapot Demo"};
             Tabs.Add(model);
             ActiveDocument = model;
+        }
+
+        public void AddTool() {
+            var model = new TempToolViewModel {Title = "Temp tool"};
+            Tools.Add(model);
         }
     }
 }
